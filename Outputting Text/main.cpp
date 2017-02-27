86b561d21ac98182c5ff7f4c442c5c160420be7e
nljor <nljor@JONSSONS>
2017-02-27 23:13:57 +0100

    Add project files.

---
+++ Outputting Text/main.cpp
@@ -0,0 +1,18 @@
+//*******************************************************
+// Name			: Outputting.cpp
+// Author		: Jörgen Jönsson
+// Version		:
+// Copyright	: Your copyright notice
+// Description	: Hellow World in C++, Ansi-style
+//*******************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello world" << endl;
+	return 0;
+}
+
+
